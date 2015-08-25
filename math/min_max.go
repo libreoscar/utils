ada00972e32aea5953014dd52a5c1a542fc816de
Rex <fdrex1987@gmail.com>
2015-08-25 00:42:55 -0700

    add min/max under math

---
+++ math/min_max.go
@@ -0,0 +1,109 @@
+package math
+
+//-------------------------- int ------------------------------------------------------------------
+
+func IntMin(a, b int) int {
+	if a <= b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func IntMax(a, b int) int {
+	if a >= b {
+		return a
+	} else {
+		return b
+	}
+}
+
+//-------------------------- uint -----------------------------------------------------------------
+
+func UIntMin(a, b uint) uint {
+	if a <= b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func UIntMax(a, b uint) uint {
+	if a >= b {
+		return a
+	} else {
+		return b
+	}
+}
+
+//-------------------------- uint64 ---------------------------------------------------------------
+
+func UInt64Min(a, b uint64) uint64 {
+	if a <= b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func UInt64Max(a, b uint64) uint64 {
+	if a >= b {
+		return a
+	} else {
+		return b
+	}
+}
+
+//-------------------------- int64 ----------------------------------------------------------------
+
+func SInt64Min(a, b int64) int64 {
+	if a <= b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func SInt64Max(a, b int64) int64 {
+	if a >= b {
+		return a
+	} else {
+		return b
+	}
+}
+
+//-------------------------- uint32 ---------------------------------------------------------------
+
+func UInt32Min(a, b uint32) uint32 {
+	if a <= b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func UInt32Max(a, b uint32) uint32 {
+	if a >= b {
+		return a
+	} else {
+		return b
+	}
+}
+
+//-------------------------- int32 ----------------------------------------------------------------
+
+func SInt32Min(a, b int32) int32 {
+	if a <= b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func SInt32Max(a, b int32) int32 {
+	if a >= b {
+		return a
+	} else {
+		return b
+	}
+}
