2b60c7757cd5365bfcf4bc3f94cf508b21c00d68
Rex <fdrex1987@gmail.com>
2015-11-07 08:10:56 -0800

    add a simple task queue

---
+++ task/queue.go
@@ -0,0 +1,20 @@
+package task
+
+type Queue struct {
+	tasks []Task
+}
+
+func (d *Queue) Add(t Task) {
+	d.tasks = append(d.tasks, t)
+}
+
+func (d *Queue) ExecAll() {
+	for _, t := range d.tasks {
+		t()
+	}
+	d.tasks = nil
+}
+
+func (d *Queue) Size() int {
+	return len(d.tasks)
+}
---
+++ task/queue_test.go
@@ -0,0 +1,23 @@
+package task
+
+import (
+	"github.com/facebookgo/ensure"
+	"testing"
+)
+
+func TestQueue(t *testing.T) {
+	n := 0
+
+	taskQueue := Queue{}
+	taskQueue.Add(func() { n += 1 })
+	taskQueue.Add(func() { n += 1 })
+	ensure.DeepEqual(t, taskQueue.Size(), 2)
+
+	taskQueue.ExecAll()
+	ensure.DeepEqual(t, taskQueue.Size(), 0)
+	ensure.DeepEqual(t, n, 2)
+
+	taskQueue.Add(func() { n += 3 })
+	taskQueue.ExecAll()
+	ensure.DeepEqual(t, n, 5)
+}
